a22b7b373702b8f3bab022e2f78d5734b2b580df
Marco Carleti <marcarleti@gmail.com>
2023-09-01 22:44:00 -0300

    bug-fix

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ function showScore() {
   resetState();
   questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
   nextButton.innerHTML = "Play Again";
-  nextButton.style.display = block;
+  nextButton.style.display = 'block';
 }
 
 function handleNextButton() {
